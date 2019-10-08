528da01d668e1ec6380f6ef0e6d4a2d256bbe3cb
AungMyo <47168280+TAWAYTHOE@users.noreply.github.com>
2019-10-08 15:39:13 +0630

    a
    other fun add

--- fffbbb.js
+++ fffbbb.js
@@ -1,3 +1,7 @@
+//https://medium.com/@tohure/dialogflow-facebook-messenger-the-story-never-told-ac033202a667 
+//https://cloud.google.com/dialogflow/docs/fulfillment-nodejs-lib
+//https://miningbusinessdata.com/category/dialogflow/
+
 'use strict';
 const dialogflow = require('dialogflow');
 const functions = require('firebase-functions');
@@ -18,8 +22,36 @@ exports.firstfunction = functions.https.onRequest ((request, response) => {
       agent.add(`HI ${name}! Welcome from first time try.`);    
   }
 
-let intentMap = new Map();	
-	intentMap.set('getName', getName);
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function other(agent) {
+    agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+    agent.add(new Card({
+        title: `Title: this is a card title`,
+        imageUrl: imageUrl,
+        text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+        buttonText: 'This is a button',
+        buttonUrl: linkUrl
+      })
+    );
+    agent.add(new Suggestion(`Quick Reply`));
+    agent.add(new Suggestion(`Suggestion`));
+    agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  }
+
+let intentMap = new Map();	           //where the intent and the function join together 
+	intentMap.set('getName', getName);   //(intent name, function name)
+  intentMap.set('Default Welcome Intent', welcome);   //please change the intent name
+  intentMap.set('fallback intent', fallback);
+  intentMap.set('other intent ', other);      
+
 agent.handleRequest(intentMap);
 		
 }); 
